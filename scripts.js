d588621a2fa0aee6cb56feff395f68ea19eac151
Coke Unity <121594464+Coke-Unity@users.noreply.github.com>
2023-11-21 13:45:26 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,53 +1,46 @@
-window.onload = function() {
-  const container = document.querySelector('.container');
-  
-  // Replace 'path/to/your/apps' with the actual path to your apps folder
-  const appsFolder = '/apps/';
-  
-  fetchAppsData(appsFolder)
-    .then(appsData => {
-      generateTiles(appsData);
-    })
-    .catch(err => {
-      console.error('Error fetching apps data:', err);
-    });
-};
+// Replace 'path/to/your/apps' with the actual relative path to your Apps folder
+const appsFolder = 'Apps';
 
 async function fetchAppsData(appsFolder) {
-  const response = await fetch(`${appsFolder}/app.json`);
-  if (response.ok) {
-    const appData = await response.json();
-    return [appData];
-  } else {
-    throw new Error(`Failed to fetch app.json: ${response.status}`);
+  try {
+    const response = await fetch(`/${appsFolder}/app.json`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching app data:', error);
+    return [];
   }
-}
-
-function generateTiles(appsData) {
-  const container = document.querySelector('.container');
-  
-  appsData.forEach(appData => {
-    const tile = createTile(appData);
-    container.appendChild(tile);
-  });
 }
 
 function createTile(appData) {
   const tile = document.createElement('div');
   tile.classList.add('tile');
-  
+
   const icon = document.createElement('img');
-  icon.src = appData.icon || 'icon.png';
-  
-  const name = document.createElement('div');
-  name.textContent = appData.name;
-  
+  icon.src = `/${appData.icon || 'icon.png'}`;
+  icon.alt = appData.name;
   tile.appendChild(icon);
+
+  const name = document.createElement('span');
+  name.textContent = appData.name;
   tile.appendChild(name);
-  
-  tile.addEventListener('click', function() {
+
+  // Add click event listener to tile
+  tile.addEventListener('click', () => {
     window.location.href = appData.url;
   });
-  
+
   return tile;
 }
+
+async function renderAppTiles() {
+  const appsData = await fetchAppsData(appsFolder);
+  const tilesContainer = document.getElementById('tiles-container');
+
+  appsData.forEach(appData => {
+    const tile = createTile(appData);
+    tilesContainer.appendChild(tile);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', renderAppTiles);
